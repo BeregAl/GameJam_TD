d4b017feada34637828bbae9aa4088e48bf8e59f
BeregAlto <antipodp@gmail.com>
2019-04-19 22:20:27 +0700

    1

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,34 +20,38 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
 
-        //сюда запишется инфо о пересечении луча, если оно будет
-        RaycastHit hit;
-        //сам луч, начинается от позиции этого объекта и направлен в сторону цели
-        Ray ray = new Ray(transform.position, target.position - transform.position);
-        //пускаем луч
-        Physics.Raycast(ray, out hit);
-
-        //если луч с чем-то пересёкся, то..
-        if (hit.collider != null)
-        {
-            //если луч не попал в цель
-            if (hit.collider.gameObject != target.gameObject)
-            {
-                Debug.Log("Путь к врагу преграждает объект: " + hit.collider.name);
-            }
-            //если луч попал в цель
-            else
-            {
-                Debug.Log("Попадаю во врага!!!");
-            }
-            //просто для наглядности рисуем луч в окне Scene
-            Debug.DrawLine(ray.origin, hit.point, Color.red);
-        }
-        /*
         if (Input.GetMouseButtonDown(0))
         {
-            GameObject go = Instantiate(towerPrefab);
-            go.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //сюда запишется инфо о пересечении луча, если оно будет
+            RaycastHit hit;
+            //сам луч, начинается от позиции этого объекта и направлен в сторону цели
+            Ray ray = new Ray(Camera.main.ScreenPointToRay(Input.mousePosition));
+            //Ray ray = new Ray(Camera.main.ScreenToWorldPoint(Input.mousePosition), target.transform.position - transform.position);
+            //пускаем луч
+            Physics.Raycast(ray, out hit);
+
+            //если луч с чем-то пересёкся, то..
+            if (hit.collider != null)
+            {
+                //если луч не попал в цель
+                if (hit.collider.gameObject != target.gameObject)
+                {
+                    Debug.Log("Путь к врагу преграждает объект: " + hit.collider.name);
+                }
+                //если луч попал в цель
+                else
+                {
+                    GameObject go = Instantiate(towerPrefab);
+                    go.transform.position = hit.point;
+                    Debug.Log("Попадаю во врага!!!");
+                }
+
+                //просто для наглядности рисуем луч в окне Scene
+                Debug.DrawLine(ray.origin, hit.point, Color.red);
+            }
+        }
+
+        /*
             
         }*/
     }
